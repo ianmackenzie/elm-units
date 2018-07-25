4302d2b6467b4e3dac9da2ebf79277274f2765df
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-25 18:53:59 -0400

    Add Angle module

---
+++ src/Angle.elm
@@ -0,0 +1,48 @@
+module Angle
+    exposing
+        ( Angle
+        , degrees
+        , inDegrees
+        , inRadians
+        , inTurns
+        , radians
+        , turns
+        )
+
+import Basics exposing ((*), (/), pi)
+import Quantity exposing (AngleUnits, Quantity(..))
+
+
+type alias Angle =
+    -- Stored as radians
+    Quantity AngleUnits
+
+
+radians : Float -> Angle
+radians numRadians =
+    Quantity numRadians
+
+
+inRadians : Angle -> Float
+inRadians (Quantity numRadians) =
+    numRadians
+
+
+degrees : Float -> Angle
+degrees numDegrees =
+    radians (pi * (numDegrees / 180))
+
+
+inDegrees : Angle -> Float
+inDegrees angle =
+    180 * (inRadians angle / pi)
+
+
+turns : Float -> Angle
+turns numTurns =
+    radians (2 * pi * numTurns)
+
+
+inTurns : Angle -> Float
+inTurns angle =
+    inRadians angle / (2 * pi)
