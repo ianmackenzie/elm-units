360b37503dfeb06b7d5a70644784cc27719e1330
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-25 18:56:37 -0400

    Add initial support for custom conversions
    Useful for modelling things like speedometers, thermometers that convert speed to angle, temperature to height etc.

--- src/Quantity.elm
+++ src/Quantity.elm
@@ -2,6 +2,7 @@ module Quantity
     exposing
         ( AccelerationUnits
         , AngleUnits
+        , Conversion
         , LengthUnits
         , Quantity(..)
         , ScreenSpace
@@ -9,6 +10,10 @@ module Quantity
         , TemperatureUnits
         , TimeUnits
         , WorldSpace
+        , convert
+        , per
+        , perDegreeCelsius
+        , perDegreeFahrenheit
         )
 
 
@@ -46,3 +51,50 @@ type TemperatureUnits
 
 type Quantity units
     = Quantity Float
+
+
+type Conversion units1 units2
+    = ConversionFactor Float
+    | PerDegreeCelsius Float
+    | PerDegreeFahrenheit Float
+
+
+per : Quantity units1 -> Quantity units2 -> Conversion units1 units2
+per (Quantity referenceValue) (Quantity equivalentValue) =
+    ConversionFactor (equivalentValue / referenceValue)
+
+
+perDegreeCelsius : Quantity units -> Conversion TemperatureUnits units
+perDegreeCelsius (Quantity scale) =
+    PerDegreeCelsius scale
+
+
+perDegreeFahrenheit : Quantity units -> Conversion TemperatureUnits units
+perDegreeFahrenheit (Quantity scale) =
+    PerDegreeFahrenheit scale
+
+
+convert : Conversion units1 units2 -> Quantity units1 -> Quantity units2
+convert conversion (Quantity value) =
+    case conversion of
+        ConversionFactor scale ->
+            Quantity (scale * value)
+
+        PerDegreeCelsius scale ->
+            -- value is in Kelvins, convert to degrees Celsius
+            let
+                degreesCelsius =
+                    value - 273.15
+            in
+            Quantity (scale * degreesCelsius)
+
+        PerDegreeFahrenheit scale ->
+            -- value is in Kelvins, convert to degrees Fahrenheit
+            let
+                degreesCelsius =
+                    value - 273.15
+
+                degreesFahrenheit =
+                    32 + 1.8 * degreesCelsius
+            in
+            Quantity (scale * degreesFahrenheit)
