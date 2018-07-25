25e53397f873416dc735a26ef255123bb8a9377e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-25 18:54:36 -0400

    Add Temperature module

---
+++ src/Temperature.elm
@@ -0,0 +1,47 @@
+module Temperature
+    exposing
+        ( Temperature
+        , celsius
+        , fahrenheit
+        , inCelsius
+        , inFahrenheit
+        , inKelvins
+        , kelvins
+        )
+
+import Quantity exposing (Quantity(..), TemperatureUnits)
+
+
+type alias Temperature =
+    -- Stored as kelvins
+    Quantity TemperatureUnits
+
+
+kelvins : Float -> Temperature
+kelvins numKelvins =
+    Quantity numKelvins
+
+
+inKelvins : Temperature -> Float
+inKelvins (Quantity numKelvins) =
+    numKelvins
+
+
+celsius : Float -> Temperature
+celsius numDegreesCelsius =
+    kelvins (273.15 + numDegreesCelsius)
+
+
+inCelsius : Temperature -> Float
+inCelsius temperature =
+    inKelvins temperature - 273.15
+
+
+fahrenheit : Float -> Temperature
+fahrenheit numDegreesFahrenheit =
+    celsius ((numDegreesFahrenheit - 32) / 1.8)
+
+
+inFahrenheit : Temperature -> Float
+inFahrenheit temperature =
+    32 + 1.8 * inCelsius temperature
