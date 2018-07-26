2bf74864daee35ff3ea5ebaa16538d09361d88b0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-26 15:02:09 -0400

    Drop conversion system
    Keep package simple...easy enough to define custom conversion functions (and probably clearer)

--- src/Quantity.elm
+++ src/Quantity.elm
@@ -2,7 +2,6 @@ module Quantity
     exposing
         ( AccelerationUnits
         , AngleUnits
-        , Conversion
         , LengthUnits
         , Quantity(..)
         , ScreenSpace
@@ -11,11 +10,7 @@ module Quantity
         , TimeUnits
         , WorldSpace
         , accelerationTimesTime
-        , convert
         , distanceOverTime
-        , per
-        , perDegreeCelsius
-        , perDegreeFahrenheit
         , speedOverTime
         , speedTimesTime
         )
@@ -55,53 +50,6 @@ type TemperatureUnits
 
 type Quantity units
     = Quantity Float
-
-
-type Conversion units1 units2
-    = ConversionFactor Float
-    | PerDegreeCelsius Float
-    | PerDegreeFahrenheit Float
-
-
-per : Quantity units1 -> Quantity units2 -> Conversion units1 units2
-per (Quantity referenceValue) (Quantity equivalentValue) =
-    ConversionFactor (equivalentValue / referenceValue)
-
-
-perDegreeCelsius : Quantity units -> Conversion TemperatureUnits units
-perDegreeCelsius (Quantity scale) =
-    PerDegreeCelsius scale
-
-
-perDegreeFahrenheit : Quantity units -> Conversion TemperatureUnits units
-perDegreeFahrenheit (Quantity scale) =
-    PerDegreeFahrenheit scale
-
-
-convert : Conversion units1 units2 -> Quantity units1 -> Quantity units2
-convert conversion (Quantity value) =
-    case conversion of
-        ConversionFactor scale ->
-            Quantity (scale * value)
-
-        PerDegreeCelsius scale ->
-            -- value is in Kelvins, convert to degrees Celsius
-            let
-                degreesCelsius =
-                    value - 273.15
-            in
-            Quantity (scale * degreesCelsius)
-
-        PerDegreeFahrenheit scale ->
-            -- value is in Kelvins, convert to degrees Fahrenheit
-            let
-                degreesCelsius =
-                    value - 273.15
-
-                degreesFahrenheit =
-                    32 + 1.8 * degreesCelsius
-            in
-            Quantity (scale * degreesFahrenheit)
 
 
 distanceOverTime : Quantity (LengthUnits space) -> Quantity TimeUnits -> Quantity (SpeedUnits space)
