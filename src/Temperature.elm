cb0e1e0c4e1bbd53042feccd539bbf41b215f7d1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-31 11:32:23 -0400

    Distinguish between 'Temperature' and 'Quantity TemperatureUnits'
    First is "a temperature" (e.g. thirty celsius), second can be considered as either a change in temperature (e.g. a temperature rise of two degrees celsius) or an absolute temperature (a number of degrees from absolute zero)

--- src/Temperature.elm
+++ src/Temperature.elm
@@ -1,50 +1,161 @@
 module Temperature
     exposing
         ( Temperature
+        , TemperatureUnits
         , celsius
+        , clamp
+        , compare
+        , degreesCelsius
+        , degreesFahrenheit
+        , difference
         , fahrenheit
+        , fromAbsolute
+        , greaterThan
         , inCelsius
+        , inDegreesCelsius
+        , inDegreesFahrenheit
         , inFahrenheit
         , inKelvins
         , kelvins
+        , lessThan
+        , max
+        , maximum
+        , min
+        , minimum
+        , sort
+        , toAbsolute
         )
 
 import Quantity exposing (Quantity(..))
 
 
-type Kelvins
+type TemperatureUnits
     = Kelvins
 
 
-type alias Temperature =
-    Quantity Kelvins
+type Temperature
+    = Temperature (Quantity TemperatureUnits)
 
 
-kelvins : Float -> Temperature
+kelvins : Float -> Quantity TemperatureUnits
 kelvins numKelvins =
     Quantity numKelvins
 
 
-inKelvins : Temperature -> Float
+inKelvins : Quantity TemperatureUnits -> Float
 inKelvins (Quantity numKelvins) =
     numKelvins
 
 
 celsius : Float -> Temperature
-celsius numDegreesCelsius =
-    kelvins (273.15 + numDegreesCelsius)
+celsius temperatureInCelsius =
+    Temperature (kelvins (273.15 + temperatureInCelsius))
 
 
 inCelsius : Temperature -> Float
-inCelsius temperature =
-    inKelvins temperature - 273.15
+inCelsius (Temperature quantity) =
+    inKelvins quantity - 273.15
 
 
 fahrenheit : Float -> Temperature
-fahrenheit numDegreesFahrenheit =
-    celsius ((numDegreesFahrenheit - 32) / 1.8)
+fahrenheit temperatureInFahrenheit =
+    celsius ((temperatureInFahrenheit - 32) / 1.8)
 
 
 inFahrenheit : Temperature -> Float
 inFahrenheit temperature =
     32 + 1.8 * inCelsius temperature
+
+
+toAbsolute : Temperature -> Quantity TemperatureUnits
+toAbsolute (Temperature quantity) =
+    quantity
+
+
+fromAbsolute : Quantity TemperatureUnits -> Temperature
+fromAbsolute quantity =
+    Temperature quantity
+
+
+degreesCelsius : Float -> Quantity TemperatureUnits
+degreesCelsius numDegreesCelsius =
+    kelvins numDegreesCelsius
+
+
+inDegreesCelsius : Quantity TemperatureUnits -> Float
+inDegreesCelsius quantity =
+    inKelvins quantity
+
+
+degreesFahrenheit : Float -> Quantity TemperatureUnits
+degreesFahrenheit numDegreesFahrenheit =
+    kelvins (numDegreesFahrenheit / 1.8)
+
+
+inDegreesFahrenheit : Quantity TemperatureUnits -> Float
+inDegreesFahrenheit quantity =
+    inKelvins quantity * 1.8
+
+
+
+-- Math with Temperature values
+
+
+lessThan : Temperature -> Temperature -> Bool
+lessThan (Temperature secondQuantity) (Temperature firstQuantity) =
+    Quantity.lessThan secondQuantity firstQuantity
+
+
+greaterThan : Temperature -> Temperature -> Bool
+greaterThan (Temperature secondQuantity) (Temperature firstQuantity) =
+    Quantity.greaterThan secondQuantity firstQuantity
+
+
+clamp : Temperature -> Temperature -> Temperature -> Temperature
+clamp (Temperature lowerQuantity) (Temperature upperQuantity) (Temperature quantity) =
+    Temperature (Quantity.clamp lowerQuantity upperQuantity quantity)
+
+
+compare : Temperature -> Temperature -> Order
+compare (Temperature firstQuantity) (Temperature secondQuantity) =
+    Quantity.compare firstQuantity secondQuantity
+
+
+min : Temperature -> Temperature -> Temperature
+min (Temperature firstQuantity) (Temperature secondQuantity) =
+    Temperature (Quantity.min firstQuantity secondQuantity)
+
+
+max : Temperature -> Temperature -> Temperature
+max (Temperature firstQuantity) (Temperature secondQuantity) =
+    Temperature (Quantity.max firstQuantity secondQuantity)
+
+
+difference : Temperature -> Temperature -> Quantity TemperatureUnits
+difference (Temperature firstQuantity) (Temperature secondQuantity) =
+    Quantity.difference firstQuantity secondQuantity
+
+
+minimum : List Temperature -> Maybe Temperature
+minimum temperatures =
+    case temperatures of
+        first :: rest ->
+            Just (List.foldl min first rest)
+
+        [] ->
+            Nothing
+
+
+maximum : List Temperature -> Maybe Temperature
+maximum temperatures =
+    case temperatures of
+        first :: rest ->
+            Just (List.foldl max first rest)
+
+        [] ->
+            Nothing
+
+
+sort : List Temperature -> List Temperature
+sort temperatures =
+    List.sortWith compare temperatures
