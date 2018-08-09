42521c3eebeead1c3713c5e24fbe334c2c5bc92b
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-08-09 13:21:20 -0400

    Add specialized spatial vector modules
    Allows properly type-checked operations on spatial vectors (vector equivalents to Length, Speed and Acceleration)

---
+++ examples/Displacement2d.elm
@@ -0,0 +1,42 @@
+module Displacement2d
+    exposing
+        ( Displacement2d
+        , convert
+        , from
+        )
+
+import Length exposing (LengthUnits)
+import Point2d exposing (Point2d)
+import Quantity
+import Vector2d exposing (Vector2d)
+
+
+type alias Displacement2d space =
+    Vector2d (LengthUnits space)
+
+
+from : Point2d space -> Point2d space -> Displacement2d space
+from startPoint endPoint =
+    let
+        ( x1, y1 ) =
+            Point2d.coordinates startPoint
+
+        ( x2, y2 ) =
+            Point2d.coordinates endPoint
+    in
+    Vector2d.fromComponents
+        ( Quantity.difference x2 x1
+        , Quantity.difference y2 y1
+        )
+
+
+convert : Length.Conversion sourceSpace destinationSpace -> Displacement2d sourceSpace -> Displacement2d destinationSpace
+convert sourceToDestination displacement =
+    let
+        ( dx, dy ) =
+            Vector2d.components displacement
+    in
+    Vector2d.fromComponents
+        ( Length.convert sourceToDestination dx
+        , Length.convert sourceToDestination dy
+        )
