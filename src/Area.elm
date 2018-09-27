44f1fd3b7577035664516313964dbff7c8807df4
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-09-27 18:10:52 -0400

    Document and expose Area module

--- src/Area.elm
+++ src/Area.elm
@@ -1,135 +1,188 @@
 module Area exposing
-    ( Area
-    , SquareMeters
-    , acres
-    , hectares
-    , inAcres
-    , inHectares
-    , inSquareCentimeters
-    , inSquareFeet
-    , inSquareInches
-    , inSquareKilometers
-    , inSquareMeters
-    , inSquareMiles
-    , inSquareMillimeters
-    , inSquareYards
-    , squareCentimeters
-    , squareFeet
-    , squareInches
-    , squareKilometers
-    , squareMeters
-    , squareMiles
-    , squareMillimeters
-    , squareYards
+    ( Area, SquareMeters
+    , squareMeters, inSquareMeters
+    , squareMillimeters, inSquareMillimeters, squareCentimeters, inSquareCentimeters, hectares, inHectares, squareKilometers, inSquareKilometers
+    , squareInches, inSquareInches, squareFeet, inSquareFeet, squareYards, inSquareYards, acres, inAcres, squareMiles, inSquareMiles
     )
+
+{-| An `Area` represents an area in square meters, square feet, acres, hectares
+etc. It is stored as a number of square meters.
+
+Note that you can construct an `Area` value directly using the functions in this
+module, but it also works to call [`Quantity.squared`](Quantity#squared) on a
+`Length` or [`Quantity.product`](Quantity#product) on a pair of `Length`s. The
+following are all equivalent:
+
+    Area.squareFeet 100
+
+    Quantity.squared (Length.feet 10)
+
+    Quantity.product (Length.feet 25) (Length.feet 4)
+
+@docs Area, SquareMeters
+
+
+## Metric
+
+@docs squareMeters, inSquareMeters
+@docs squareMillimeters, inSquareMillimeters, squareCentimeters, inSquareCentimeters, hectares, inHectares, squareKilometers, inSquareKilometers
+
+
+## Imperial
+
+@docs squareInches, inSquareInches, squareFeet, inSquareFeet, squareYards, inSquareYards, acres, inAcres, squareMiles, inSquareMiles
+
+-}
 
 import Length exposing (Meters)
 import Quantity exposing (Quantity(..), Squared)
 
 
+{-| -}
 type alias SquareMeters =
     Squared Meters
 
 
+{-| -}
 type alias Area =
     Quantity Float SquareMeters
 
 
+{-| Construct an area from a number of square meters.
+-}
 squareMeters : Float -> Area
 squareMeters numSquareMeters =
     Quantity numSquareMeters
 
 
+{-| Convert an area to a number of square meters.
+-}
 inSquareMeters : Area -> Float
 inSquareMeters (Quantity numSquareMeters) =
     numSquareMeters
 
 
+{-| Construct an area from a number of square millimeters.
+-}
 squareMillimeters : Float -> Area
 squareMillimeters numSquareMillimeters =
     squareMeters (1.0e-6 * numSquareMillimeters)
 
 
+{-| Convert an area to a number of square millimeters.
+-}
 inSquareMillimeters : Area -> Float
 inSquareMillimeters area =
     1.0e6 * inSquareMeters area
 
 
+{-| Construct an area from a number of square inches.
+-}
 squareInches : Float -> Area
 squareInches numSquareInches =
     squareMeters (0.0254 * 0.0254 * numSquareInches)
 
 
+{-| Convert an area to a number of square inches.
+-}
 inSquareInches : Area -> Float
 inSquareInches area =
     inSquareMeters area / (0.0254 * 0.0254)
 
 
+{-| Construct an area from a number of square centimeters.
+-}
 squareCentimeters : Float -> Area
 squareCentimeters numSquareCentimeters =
     squareMeters (1.0e-4 * numSquareCentimeters)
 
 
+{-| Convert an area to a number of square centimeters.
+-}
 inSquareCentimeters : Area -> Float
 inSquareCentimeters area =
     1.0e4 * inSquareMeters area
 
 
+{-| Construct an area from a number of square feet.
+-}
 squareFeet : Float -> Area
 squareFeet numSquareFeet =
     squareMeters (0.3048 * 0.3048 * numSquareFeet)
 
 
+{-| Convert an area to a number of square feet.
+-}
 inSquareFeet : Area -> Float
 inSquareFeet area =
     inSquareMeters area / (0.3048 * 0.3048)
 
 
+{-| Construct an area from a number of square yards.
+-}
 squareYards : Float -> Area
 squareYards numSquareYards =
     squareMeters (0.9144 * 0.9144 * numSquareYards)
 
 
+{-| Convert an area to a number of square yards.
+-}
 inSquareYards : Area -> Float
 inSquareYards area =
     inSquareMeters area / (0.9144 * 0.9144)
 
 
+{-| Construct an area from a number of hectares.
+-}
 hectares : Float -> Area
 hectares numHectares =
     squareMeters (1.0e4 * numHectares)
 
 
+{-| Convert an area to a number of hectares.
+-}
 inHectares : Area -> Float
 inHectares area =
     1.0e-4 * inSquareMeters area
 
 
+{-| Construct an area from a number of square kilometers.
+-}
 squareKilometers : Float -> Area
 squareKilometers numSquareKilometers =
     squareMeters (1.0e6 * numSquareKilometers)
 
 
+{-| Convert an area to a number of square kilometers.
+-}
 inSquareKilometers : Area -> Float
 inSquareKilometers area =
     1.0e-6 * inSquareMeters area
 
 
+{-| Construct an area from a number of acres.
+-}
 acres : Float -> Area
 acres numAcres =
     squareMeters (4046.8564224 * numAcres)
 
 
+{-| Convert an area to a number of acres.
+-}
 inAcres : Area -> Float
 inAcres area =
     inSquareMeters area / 4046.8564224
 
 
+{-| Construct an area from a number of square miles.
+-}
 squareMiles : Float -> Area
 squareMiles numSquareMiles =
     squareMeters (1609.344 * 1609.344 * numSquareMiles)
 
 
+{-| Convert an area to a number of square miles.
+-}
 inSquareMiles : Area -> Float
 inSquareMiles area =
     inSquareMeters area / (1609.344 * 1609.344)
