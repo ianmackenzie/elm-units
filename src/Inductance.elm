7accc7c63c5f4275aad0dab411b29b502edc486c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-10-17 23:02:42 -0400

    Tweak documentation wording/formatting
    More consistent with other elm-units modules

--- src/Inductance.elm
+++ src/Inductance.elm
@@ -4,7 +4,7 @@ module Inductance exposing
     , microhenries, inMicrohenries, nanohenries, inNanohenries, kilohenries, inKilohenries
     )
 
-{-| A `Inductance` value represents an inductance in henries [H].
+{-| A `Inductance` value represents an electrical inductance in henries.
 
 @docs Inductance, Henries
 @docs henries, inHenries, millihenries, inMillihenries
@@ -28,70 +28,70 @@ type alias Inductance =
     Quantity Float Henries
 
 
-{-| Construct an inductance from a number of henries
+{-| Construct an inductance from a number of henries.
 -}
 henries : Float -> Inductance
 henries numHenries =
     Quantity numHenries
 
 
-{-| Convert an inductance to a number of henries
+{-| Convert an inductance to a number of henries.
 -}
 inHenries : Inductance -> Float
 inHenries (Quantity numHneries) =
     numHneries
 
 
-{-| Construct an inductance from a number of millihenries
+{-| Construct an inductance from a number of millihenries.
 -}
 millihenries : Float -> Inductance
 millihenries numMilliHenries =
     henries (numMilliHenries * 1.0e-3)
 
 
-{-| Convert an inductance to a number of millihenries
+{-| Convert an inductance to a number of millihenries.
 -}
 inMillihenries : Inductance -> Float
 inMillihenries inductance =
     inHenries inductance / 1.0e-3
 
 
-{-| Construct an inductance from a number of microhenries
+{-| Construct an inductance from a number of microhenries.
 -}
 microhenries : Float -> Inductance
 microhenries numMicroHenries =
     henries (numMicroHenries * 1.0e-6)
 
 
-{-| Convert an inductance to a number of microhenries
+{-| Convert an inductance to a number of microhenries.
 -}
 inMicrohenries : Inductance -> Float
 inMicrohenries inductance =
     inHenries inductance / 1.0e-6
 
 
-{-| Construct an inductance from a number of nanohenries
+{-| Construct an inductance from a number of nanohenries.
 -}
 nanohenries : Float -> Inductance
 nanohenries numNanoHenries =
     henries (numNanoHenries * 1.0e-9)
 
 
-{-| Convert an inductance to a number of nanohenries
+{-| Convert an inductance to a number of nanohenries.
 -}
 inNanohenries : Inductance -> Float
 inNanohenries inductance =
     inHenries inductance / 1.0e-9
 
 
-{-| Construct an inductance from a number of kilohenries
+{-| Construct an inductance from a number of kilohenries.
 -}
 kilohenries : Float -> Inductance
 kilohenries numKiloHenries =
     henries (numKiloHenries * 1.0e3)
 
 
-{-| Convert an inductance to a number of kilohenries
+{-| Convert an inductance to a number of kilohenries.
 -}
 inKilohenries : Inductance -> Float
 inKilohenries inductance =
