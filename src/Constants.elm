988835bcfc3a09b66e71aa770745662227422de0
katjam <katja@neontribe.co.uk>
2019-04-06 12:31:32 +0100

    Better source doc for imperial gallan constant

--- src/Constants.elm
+++ src/Constants.elm
@@ -72,10 +72,13 @@ liter =
     0.001 * cubicMeter
 
 
+{-| Sourced from [UK Weights and Measures Act][1]. One imperial gallon is equal to
+4.54609 cubic decimeters (formerly defined as the volume of one kilogram
+of pure water under standard conditions, now equal to 1 liter.
 
--- Sourced from UK Weights and Measures Act
+[1]: https://www.legislation.gov.uk/ukpga/1985/72#tgp10-tbl10-tbd1-tr22
 
-
+-}
 imperialGallon : Float
 imperialGallon =
     4.54609 * liter
