8c878adde50b4b73c7bcd31ee058553058718d20
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-06-22 19:31:35 -0400

    Add Pixels.[int, float, toInt, toFloat]
    More readable/explicit than Pixels.pixels/Pixels.inPixels

--- src/Pixels.elm
+++ src/Pixels.elm
@@ -1,30 +1,45 @@
 module Pixels exposing
-    ( Pixels, PixelsPerSecond, PixelsPerSecondSquared, SquarePixels
-    , pixels, inPixels, pixel
+    ( Pixels
+    , int, float, toInt, toFloat, pixels, inPixels, pixel
+    , PixelsPerSecond, PixelsPerSecondSquared
     , pixelsPerSecond, inPixelsPerSecond
     , pixelsPerSecondSquared, inPixelsPerSecondSquared
+    , SquarePixels
     , squarePixels, inSquarePixels
     )
 
 {-| Although most of the focus of `elm-units` is on physical/scientific units,
-it's very useful to be able to safely convert back and forth between (for
+it's often useful to be able to safely convert back and forth between (for
 example) [`Length`](Length) values in the real world and on-screen lengths in
-pixels. This module provides a standard `Pixels` units type and basic functions
-for constructing/converting values of type `Quantity number Pixels`, which
-allows you to do things like represent conversions between real-world and
-on-screen lengths as [rates of change][1]. This in turn means that all the
-normal `Quantity` functions can be used to convert between pixels and other
-units, or even do type-safe math directly on pixel values.
+pixels.
+
+This module provides a standard `Pixels` units type and basic functions for
+constructing/converting values of type `Quantity Int Pixels` or
+`Quantity Float Pixels`, which allows you to do things like represent
+conversions between real-world and on-screen lengths as [rates of change][1].
+This in turn means that all the normal [`Quantity`](Quantity) functions can be
+used to convert between pixels and other units, or even do type-safe math
+directly on pixel values.
 
 [1]: Quantity#working-with-rates
 
-@docs Pixels, PixelsPerSecond, PixelsPerSecondSquared, SquarePixels
+@docs Pixels
 
-@docs pixels, inPixels, pixel
+@docs int, float, toInt, toFloat, pixels, inPixels, pixel
+
+
+## Rates
+
+@docs PixelsPerSecond, PixelsPerSecondSquared
 
 @docs pixelsPerSecond, inPixelsPerSecond
 
 @docs pixelsPerSecondSquared, inPixelsPerSecondSquared
+
+
+## Areas
+
+@docs SquarePixels
 
 @docs squarePixels, inSquarePixels
 
@@ -60,12 +75,60 @@ type alias SquarePixels =
     Squared Pixels
 
 
-{-| Construct a quantity representing a given number of on-screen pixels:
+{-| Construct a quantity representing an integer number of on-screen pixels:
 
     screenWidth =
-        pixels 1920
+        Pixels.int 1920
 
-Note that passing an `Int` will give you a
+-}
+int : Int -> Quantity Int Pixels
+int numPixels =
+    Quantity numPixels
+
+
+{-| Construct a quantity representing a floating-point number of on-screen
+pixels:
+
+    lineWeight =
+        Pixels.float 1.5
+
+-}
+float : Float -> Quantity Float Pixels
+float numPixels =
+    Quantity numPixels
+
+
+{-| Convert an integer number of pixels back into a plain `Int`:
+
+    Pixels.int 1920
+        |> Quantity.multiplyBy 2
+        |> Pixels.toInt
+    --> 3840
+
+-}
+toInt : Quantity Int Pixels -> Int
+toInt (Quantity numPixels) =
+    numPixels
+
+
+{-| Convert a floating-point number of pixels back into a plain `Float`:
+
+    pixelDensity =
+        Pixels.float 96 |> Quantity.per (Length.inches 1)
+
+    Length.centimeters 1
+        |> Quantity.at pixelDensity
+        |> Pixels.toFloat
+    --> 37.795275590551185
+
+-}
+toFloat : Quantity Float Pixels -> Float
+toFloat (Quantity numPixels) =
+    numPixels
+
+
+{-| Generic version of `Pixels.int`/`Pixels.float`, for consistency with other
+modules like `Length`. Note that passing an `Int` will give you a
 
     Quantity Int Pixels
 
@@ -73,8 +136,9 @@ while passing a `Float` will give you a
 
     Quantity Float Pixels
 
-If you pass a _literal_ integer like `1920`, the result can be used as either an
-`Int` _or_ `Float` number of pixels.
+If you pass a _literal_ integer like `1920`, you will get a generic `Quantity
+number Pixels` which can be used as either an `Int` _or_ `Float` number of
+pixels.
 
 -}
 pixels : number -> Quantity number Pixels
@@ -82,16 +146,8 @@ pixels numPixels =
     Quantity numPixels
 
 
-{-| Convert a `Pixels` value to a plain number of pixels.
-
-    pixelDensity =
-        pixels 96 |> Quantity.per (Length.inches 1)
-
-    Length.centimeters 1
-        |> Quantity.at pixelDensity
-        |> inPixels
-    --> 37.795275590551185
-
+{-| Convert a `Pixels` value to a plain number of pixels. This is a generic
+version of `Pixels.toInt`/`Pixels.toFloat`.
 -}
 inPixels : Quantity number Pixels -> number
 inPixels (Quantity numPixels) =
@@ -119,12 +175,12 @@ pixelsPerSecond numPixelsPerSecond =
         Duration.milliseconds 16
 
     dragDistance =
-        pixels 2
+        Pixels.float 2
 
     dragSpeed =
         dragDistance |> Quantity.per elapsedTime
 
-    dragSpeed |> inPixelsPerSecond
+    dragSpeed |> Pixels.inPixelsPerSecond
     --> 125
 
 -}
@@ -158,9 +214,9 @@ squarePixels numSquarePixels =
 {-| Convert an on-screen area to a number of square pixels.
 
     area =
-        pixels 1920 |> Quantity.times (pixels 1080)
+        Pixels.int 1928 |> Quantity.times (Pixels.int 1080)
 
-    area |> inSquarePixels
+    area |> Pixels.inSquarePixels
     --> 2073600
 
 -}
